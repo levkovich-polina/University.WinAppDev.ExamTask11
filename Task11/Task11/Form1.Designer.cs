92ff9dcea2efc09b2f7f3e2574029f65d4481f7e
levkovich-polina <levkovich.polina@gmail.com>
2023-02-18 00:36:47 +0300

    Implemented the game

---
+++ Task11/Task11/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Task11
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Panel = new System.Windows.Forms.Panel();
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Panel
+            // 
+            this.Panel.BackColor = System.Drawing.Color.White;
+            this.Panel.Location = new System.Drawing.Point(1, 0);
+            this.Panel.Name = "Panel";
+            this.Panel.Size = new System.Drawing.Size(620, 620);
+            this.Panel.TabIndex = 0;
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.Location = new System.Drawing.Point(654, 41);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(136, 54);
+            this.PlayButton.TabIndex = 1;
+            this.PlayButton.Text = "Начать игру";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(654, 136);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(136, 54);
+            this.StopButton.TabIndex = 2;
+            this.StopButton.Text = "Остановить игру";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(822, 620);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.PlayButton);
+            this.Controls.Add(this.Panel);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel Panel;
+        private Button PlayButton;
+        private Button StopButton;
+    }
+}
---
+++ Task11/Task11/Form1.cs
@@ -0,0 +1,174 @@
+using Timer = System.Threading.Timer;
+
+namespace Task11
+{
+    public partial class Form1 : Form
+    {
+
+        public class Square
+        {
+            public int PositionX { get; set; }
+            public int PositionY { get; set; }
+
+            public int Width { get; set; }
+            public int Height { get; set; }
+
+            public Color Color { get; set; }
+            public Square(int x, int y, int width, int height, Color color)
+            {
+                PositionX = x;
+                PositionY = y;
+                Width = width;
+                Height = height;
+                Color = color;
+            }
+        }
+        public class Player
+        {
+            public int PositionPlayerX { get; set; }
+            public int PositionPlayerY { get; set; }
+
+            public int Width { get; set; }
+            public int Height { get; set; }
+
+            public Color Color { get; set; }
+            public Player(int x, int y, int width, int height, Color color)
+            {
+                PositionPlayerX = x;
+                PositionPlayerY = y;
+                Width = width;
+                Height = height;
+                Color = color;
+            }
+        }
+        List<Square> _squares = new List<Square>();
+        private Timer _timer;
+        Random _random = new Random();
+        int _positionPlayerX;
+        int _positionPlayerY;
+        Player _player;
+        List<int> _listPositionX = new List<int>();
+        int _positionX;
+        int _positionY;
+        private const int _gameFieldSize = 30;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            Panel.CreateGraphics().Clear(Color.White);
+            _squares.Clear();
+            _positionPlayerX = 15;
+            _positionPlayerY = 30;
+
+            _player = new Player(_positionPlayerX, _positionPlayerY, 1, 1, Color.Red);
+
+            TimerCallback tm = new TimerCallback(OnTimerTicked);
+            _timer = new Timer(tm, 0, 0, 200);
+        }
+        public void OnTimerTicked(object obj)
+        {
+            _listPositionX.Clear();
+
+            for (int j = 0; j < _gameFieldSize; j++)
+            {
+                int chance = _random.Next(1, 11);
+                if (chance <= 1)
+                {
+                    _listPositionX.Add(j);
+                }
+            }
+            if (_squares.Count > 1)
+            {
+                for (int i = 0; i < _squares.Count; i++)
+                {
+                    if (_player.PositionPlayerY - 1 == _squares[i].PositionY && _player.PositionPlayerX <= _squares[i].PositionX && _squares[i].PositionX <= (_player.PositionPlayerX + _player.Width - 1))
+                    {
+                        _timer.Dispose();
+                        MessageBox.Show("Вы врезались в огорождение! Игра окончена.");
+                    }
+                }
+            }
+            if (_squares.Count > 1)
+            {
+                for (int i = 0; i < _squares.Count; i++)
+                {
+                    _squares[i].PositionY++;
+                }
+            }
+            _positionY = 0;
+            for (int i = 0; i < _listPositionX.Count; i++)
+            {
+                _positionX =  _listPositionX[i];
+
+                Square square = new Square(_positionX, _positionY, 1, 1, Color.Gray);
+                _squares.Add(square);
+            }
+            Draw();
+        }
+
+        private void Draw()
+        {
+            Panel.CreateGraphics().Clear(Color.White);
+            Graphics g = Panel.CreateGraphics();
+            int width = Panel.ClientSize.Width / _gameFieldSize;
+            int height = Panel.ClientSize.Height / _gameFieldSize;
+            for (int i = 0; i < _squares.Count; i++)
+            {
+                var dx = _squares[i].PositionX * width;
+                var dy = _squares[i].PositionY * height;
+                var dWidth = _squares[i].Width * width;
+                var dHeight = _squares[i].Height * height;
+                var brush = _squares[i].Color;
+                Invoke(() =>
+                {
+                    g.FillRectangle(new SolidBrush(Color.White), dx, dy - height, dWidth, dHeight);
+
+                    g.FillRectangle(new SolidBrush(brush), dx, dy, dWidth, dHeight);
+                });
+            }
+            g.FillRectangle(new SolidBrush(_player.Color), _player.PositionPlayerX * width, _player.PositionPlayerY * height, _player.Width * width, _player.Height * height);
+
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            _timer.Dispose();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                if (_player.PositionPlayerX > 0)
+                {
+                    _player.PositionPlayerX--;
+                }
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                if (_player.PositionPlayerX < _gameFieldSize)
+                {
+                    _player.PositionPlayerX++;
+                }
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                if (_player.PositionPlayerY > 0)
+                {
+                    _player.PositionPlayerY--;
+                }
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                if (_player.PositionPlayerY < _gameFieldSize)
+                {
+                    _player.PositionPlayerY++;
+                }
+            }
+        }
+    }
+}
